9f7914f47b4ff1a73140db3723b94caa058ef1d4
FreeMan <zelonaire@gmail.com>
2023-03-08 03:15:18 +0000

    changing the text content on the card when a user starts typing in an input

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 'use strict'
 
 //Variables
-const cardDetails = document.querySelector(".card-details");
+const cardDetails = document.querySelectorAll(".card-details");
 const cardInputs = document.querySelectorAll("input");
+console.log(cardDetails)
+console.log(cardInputs);
+
+
+//INPUT FUNCTION
+//A function to replace the text content on the card when a user starts typing in any of the input
+//Each index of the input corresponds to its index of the elements on the card
+const displayDetailsOnCard = function (index) {
+    cardDetails[index].textContent = cardInputs[index].value;
+};
+
+//Looping through the input fields to get each index
+for (let i = 0; i < cardInputs.length; i++){
+  cardInputs[i].addEventListener('input', () => {
+    displayDetailsOnCard(i)
+  })
+}
